6722ad0ed2e235a5630f8155a979822c4fbc90f3
Sparky <384sparky@gmail.com>
2013-01-23 19:02:31 -0500

    Testing accelerometer... Getting started.

--- Sparky.cpp
+++ Sparky.cpp
@@ -10,16 +10,14 @@ class RobotDemo : public SimpleRobot
 {
 	RobotDrive myRobot; // robot drive system
 	Joystick stick1, stick2; // only joystick
-	Gyro gyro;
-	Accelerometer acc;
+	//Accelerometer acc;
+	//I2C acc2;
 
 public:
 	RobotDemo(void):
 		myRobot(2, 3),	// these must be initialized in the same order
 		stick1(1),		// as they are declared above.
-		stick2(2),
-		gyro(8),
-		acc(2)
+		stick2(2)
 	{
 		myRobot.SetExpiration(0.1);
 	}
@@ -32,6 +30,7 @@ public:
 		myRobot.SetSafetyEnabled(false);
 		while(IsAutonomous()) // this is a change
 		{
+			/*
 			gyro.Reset();
 			float angle = gyro.GetAngle() + 45; // get the angle from the gyro
 			myRobot.Drive(0.5, -angle / 30);
@@ -41,6 +40,7 @@ public:
 			dsLCD->PrintfLine(DriverStationLCD::kUser_Line1, "Gyro Value: %f", gyro.GetAngle());
 			dsLCD->PrintfLine(DriverStationLCD::kUser_Line2, "Acceleration: %f", acc.GetAcceleration());
 			dsLCD->UpdateLCD();
+			*/
 			Wait(2.0);
 		}
 		myRobot.Drive(0.0, 0.0);
@@ -52,10 +52,35 @@ public:
 	void OperatorControl(void)
 	{
 		myRobot.SetSafetyEnabled(true);
+		
+		/*
+		DigitalModule *dm = DigitalModule::GetInstance(2);
+		I2C *isc = dm->GetI2C(0x3A);
+		isc->Write(0x2D, 0x08);
+		*/
+		
+		ADXL345_I2C a(2);
+		
 		while (true)
 		{
 			myRobot.SetInvertedMotor(myRobot.kRearLeftMotor, false);
 			myRobot.SetInvertedMotor(myRobot.kRearRightMotor, false);
+			
+			/*
+			DriverStationLCD *dsLCD = DriverStationLCD::GetInstance();
+			dsLCD->PrintfLine(DriverStationLCD::kUser_Line1, "Acceleration: ", acc.GetAcceleration());
+			dsLCD->UpdateLCD();
+			*/
+			
+			DriverStationLCD *dsLCD = DriverStationLCD::GetInstance();
+			dsLCD->PrintfLine(DriverStationLCD::kUser_Line1, "Acceleration X: ",
+					a.GetAcceleration(a.kAxis_X));
+			dsLCD->PrintfLine(DriverStationLCD::kUser_Line2, "Acceleration Y: ",
+					a.GetAcceleration(a.kAxis_Y));
+			dsLCD->PrintfLine(DriverStationLCD::kUser_Line3, "Acceleration Z: ",
+					a.GetAcceleration(a.kAxis_Z));
+			dsLCD->UpdateLCD();
+			
 			if((stick1.GetTrigger() == true) && (stick2.GetTrigger() == true))
 			{
 				myRobot.TankDrive(stick1,stick2);
@@ -68,14 +93,6 @@ public:
 			{
 				myRobot.ArcadeDrive(stick2);
 			}
-			else{}
-			
-			float angle = gyro.GetAngle();
-			float speed = acc.GetAcceleration();
-			DriverStationLCD *dsLCD = DriverStationLCD::GetInstance();
-			dsLCD->PrintfLine(DriverStationLCD::kUser_Line1, "Gyro Angle: ", angle);
-			dsLCD->PrintfLine(DriverStationLCD::kUser_Line2, "Acceleration: ", speed);
-			dsLCD->UpdateLCD();
 			Wait(0.005);				// wait for a motor update time
 		}
 	}
